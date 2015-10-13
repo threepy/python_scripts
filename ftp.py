847e845d439fc268b00e855ea58978167886c030
threepy <1476697149@qq.com>
2015-10-13 13:24:37 +0800

    add ftp.py

---
+++ ftp.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+import ftplib
+
+host = '192.168.100.145'
+username = 'dev'
+password = 'dev'
+remotefile = 'upload1.MOV'
+localfile = 'download.MOV'
+
+
+try:
+	# get ftp object and login
+	ftp = ftplib.FTP(host)
+	ftp.login(username, password)
+	print ftp.getwelcome()
+except ftplib.all_errors as e:
+	print u'ftp 连接失败', e
+
+# download file as ascii mode
+def downloadfile_ascii(localfile, remotefile):
+	# open local file
+	f = open(localfile, 'wb')
+	# open remote file and write to local file
+	ftp.retrlines('RETR ' + remotefile, f.write)
+	f.close()
+	ftp.close()
+	print u'下载完成'
+# download file as binary mode
+def downloadfile_bin(localfile, remotefile):
+	f = open(localfile, 'wb')
+	ftp.retrbinary('RETR ' + remotefile, f.write)
+	f.close()
+	ftp.close()
+	print u'download ok'
+
+# upload file as binary mode
+def uploadfile_bin(localile):
+	f = open(localfile, 'rb')
+	ftp.storbinary('STOR ' + localfile, f)
+	f.close()
+	ftp.quit()
+	print 'upload ok'
+
+if __name__ == "__main__":
+	 uploadfile_bin(localfile)
+
